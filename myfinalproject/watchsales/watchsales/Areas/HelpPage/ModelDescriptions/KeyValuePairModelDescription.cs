554634dcabc5a929eddaea6946eeb04b07fbf919
badr mansour <badr.mansour85@gmail.com>
2015-11-10 17:17:45 -0400

    the final web page asp.net

---
+++ myfinalproject/watchsales/watchsales/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace watchsales.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
