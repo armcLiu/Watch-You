9197867d4ce2d7c44c53fbd748deb1302ee206e2
Satish Chandra Panigrahi <Satish Chandra Panigrahi>
2015-11-17 12:53:40 -0400

    Down Load

--- www/WatchDataBase/WatchSite/Default2.aspx.cs
+++ www/WatchDataBase/WatchSite/Default2.aspx.cs
@@ -5,80 +5,77 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
-using System.Data.SqlClient;
+using System.Data.SqlClient; // ADO.NET code
 using System.Configuration;
+
 
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {      
+    {
         if (!IsPostBack)
         {
             SetImageUrl();
         }
-
-       // SetImageUrl();
     }
 
     protected void Timer1_Tick(object sender, EventArgs e)
     {
-        try
-        {
-            int i = (int)ViewState["ImageDisplayed"];
-            i = i + 1;
-           // EnableViewState = true;
-            ViewState["ImageDisplayed"] = i;
+        int i = (int)ViewState["ImageDisplayed"];
+        i = i + 1;
+        EnableViewState = true;
+        ViewState["ImageDisplayed"] = i;
 
-            DataRow imageDataRow = ((DataSet)ViewState["ImageData"]).Tables["image"].Select().FirstOrDefault(x => x["order"].ToString() == i.ToString());
-            if (imageDataRow != null)
-            {
-                ImageButton1.ImageUrl = "~/Images/" + imageDataRow["name"].ToString();
-                lblImageName.Text = imageDataRow["name"].ToString();
-                lblImageOrder.Text = imageDataRow["order"].ToString();
-            }
-            else
-            {
-                SetImageUrl();
-            }
-        }
-        catch (Exception Ex1)
+        DataRow imageDataRow = ((DataSet)ViewState["ImageData"]).Tables["image"].Select().FirstOrDefault(x => x["order"].ToString() == i.ToString());
+        if (imageDataRow != null)
         {
-            lblImageName.Text = Ex1.Message;
-            
+            ImageButton1.ImageUrl = "~/Images/" + imageDataRow["name"].ToString();
+            lblImageName.Text = imageDataRow["name"].ToString();
+            lblImageOrder.Text = imageDataRow["order"].ToString();
         }
-       
+        else
+        {
+            SetImageUrl();
+        }
     }
 
     private void SetImageUrl()
     {
-
+        
         try
         {
             DataSet ds = new DataSet();
-            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-
+            string CS = System.Configuration.ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            // string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
             SqlConnection con = new SqlConnection(CS);
             SqlDataAdapter da = new SqlDataAdapter("spGetImageData", con);
-            da.Fill(ds, "image");
-            ViewState["ImageData"] = ds;
-            ViewState["ImageDisplayed"] = 1;
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
 
+            da.Fill(ds, "image");
+
+
+            ViewState["ImageData"] = ds;
+
+            ViewState["ImageDisplayed"] = 1;
             DataRow imageDataRow = ds.Tables["image"].Select().FirstOrDefault(x => x["order"].ToString() == "1");
             ImageButton1.ImageUrl = "~/Images/" + imageDataRow["name"].ToString();
 
             lblImageName.Text = imageDataRow["name"].ToString();
             lblImageOrder.Text = imageDataRow["order"].ToString();
+        }
+        catch(Exception Ex)
+        {
+            lblImageName.Text= Ex.Message;
+           
+        }
 
-        }
-        catch (SqlException SqlE)
-        {
-            lblImageName.Text = SqlE.Message;
-        }
-        catch (Exception Ex)
-        {
-            lblImageName.Text = Ex.Message;
-        }
+       // if (CS == null || string.IsNullOrEmpty(CS))
+        //    throw new Exception("Fatal error: missing connecting string in web.config file");
+
+
+       
     }
+
     protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
     {
         if(Timer1.Enabled)
